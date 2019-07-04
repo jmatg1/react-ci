3a3062a3533a8346cd6a92112b93bee03498ff21
Roman <saroman96@mail.ru>
2019-07-04 17:01:40 +0700

    save in localStorage

--- src/middlewares/localStorage.js
+++ src/middlewares/localStorage.js
@@ -2,5 +2,14 @@ import { setItem } from '../shared/utility'
 import * as actionTypes from '../store/actions/actionTypes'
 export default (store) => (next) => (action) => {
   next(action)
-  if (action.type === actionTypes.SET_PROFILE) setItem('users', Object.values(store.getState().users.toJS()))
+  if (action.type === actionTypes.SET_PROFILE) saveUsers(store)
+  if (action.type === actionTypes.ADD_COMMENT) {
+    saveComments(store)
+    saveTweets(store)
+  }
+  if (action.type === actionTypes.ADD_TWEET) saveTweets(store)
 }
+
+const saveComments = (store) => setItem('comments', Object.values(store.getState().comments.toJS()))
+const saveTweets = (store) => setItem('tweets', Object.values(store.getState().tweets.toJS()))
+const saveUsers = (store) => setItem('users', Object.values(store.getState().users.toJS()))
